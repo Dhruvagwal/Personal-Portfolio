cecf7393654bfe3776a970a49507bc07e52acf4a
dhruv aggarwal <dhruvgwal@gmail.com>
2025-08-16 09:57:46 +0530

    starter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-netlify';
 
 import { mdsvex, escapeSvelte } from 'mdsvex';
 import { createHighlighter } from 'shiki';
